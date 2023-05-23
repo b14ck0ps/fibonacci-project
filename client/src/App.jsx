c63ae305c604a77e87333eb21b4883ebed17ee95
Azran Hossain <mr69751@gmail.com>
2023-05-24 02:03:43 +0600

    routing issue fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Fib from "./Fib";
 import OtherPage from "./OtherPage";
 
 function App() {
   return (
-    <Router>
+    <>
       <div>
         <header>
-          <Link to="/"> Home </Link>
+          <Link to="/"> Home </Link> ||{" "}
           <Link to="/otherpage"> Other Page </Link>
         </header>
       </div>
       <div>
-        <Route exact path="/" component={Fib} />
-        <Route exact path="/otherpage" component={OtherPage} />
+        <Routes>
+          <Route exact path="/" element={<Fib />} />
+          <Route exact path="/otherpage" element={<OtherPage />} />
+        </Routes>
       </div>
-    </Router>
+    </>
   );
 }
 
